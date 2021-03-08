1748b4ebc2a372a2610b9671063a91d81e2206f4
phuongtrau <damhoangphuong1211@gmail.com>
2021-03-08 13:50:23 +0700

    first commit

---
+++ tealayers/tealayer1.0/tealayers/test_mnist.py
@@ -0,0 +1,105 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+import operator
+import functools
+import math
+
+import tensorflow as tf
+import numpy as np
+from keras import backend as K
+from keras import Model
+from keras.engine.topology import Layer
+from keras import initializers
+from keras.models import Sequential
+from keras.layers import Dropout, Flatten, Activation, Input, Lambda, concatenate
+from keras.datasets import mnist
+from keras.optimizers import Adam
+from keras.utils import to_categorical
+
+from tea import Tea
+from additivepooling import AdditivePooling
+
+import sys
+sys.path.append("/home/phuongdh/Documents/SNN/RANC/software/rancutils/rancutils")
+
+from teaconversion import create_cores,create_packets
+from packet import Packet
+
+# Load MNIST data
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+# save old labels for later
+y_test_not = y_test
+
+# convert class vectors to binary class matrices
+y_train = to_categorical(y_train, 10)
+y_test = to_categorical(y_test, 10)
+
+# Define model (use functional API to follow fan-in and 
+# fan-out constraints)
+inputs = Input(shape=(28, 28,))
+flattened_inputs = Flatten()(inputs)
+# Send input into 4 different cores (256 axons each)
+x0 = Lambda(lambda x : x[:,:256])(flattened_inputs)
+x1 = Lambda(lambda x : x[:,176:432])(flattened_inputs)
+x2 = Lambda(lambda x : x[:,352:608])(flattened_inputs)
+x3 = Lambda(lambda x : x[:,528:])(flattened_inputs)
+x0 = Tea(64)(x0)
+x1 = Tea(64)(x1)
+x2 = Tea(64)(x2)
+x3 = Tea(64)(x3)
+# Concatenate output of first layer to send into next
+x = concatenate([x0, x1, x2, x3])
+x = Tea(250)(x)
+# Pool spikes and output neurons into 10 classes.
+x = AdditivePooling(10)(x)
+predictions = Activation('softmax')(x)
+
+model = Model(inputs=inputs, outputs=predictions)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=Adam(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=128,
+          epochs=1,
+          verbose=1,
+          validation_split=0.2)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+cores_sim = create_cores(model, len(model.layers), neuron_reset_type=0) 
+
+# from rancutils.teaconversion import create_cores, create_packets, Packet
+# from rancutils.output_bus import OutputBus
+# from rancutils.serialization import save as sim_save
+
+# x_test_flat = x_test.reshape((10000, 784))
+# partitioned_packets = []
+
+# # Use absolute/hard reset by specifying neuron_reset_type=0
+# cores_sim = create_cores(model, 2, neuron_reset_type=0) 
+# # Partition the packets into groups as they will be fed into each of the input cores
+# partitioned_packets.append(x_test_flat[:num_test_samples, :256])
+# partitioned_packets.append(x_test_flat[:num_test_samples, 176:432])
+# partitioned_packets.append(x_test_flat[:num_test_samples, 352:608])
+# partitioned_packets.append(x_test_flat[:num_test_samples, 528:])
+# packets_sim = create_packets(partitioned_packets)
+# output_bus_sim = OutputBus((0, 2), num_outputs=250)
+
+# # This file can then be used as an input json to the RANC Simulator through the "input file" argument.
+# sim_save("mnist_config.json", cores_sim, packets_sim, output_bus_sim, indent=2)
+# # Additionally, output the tensorflow predictions and correct labels for later cross validation
+# np.save("mnist_tf_preds.txt", test_predictions)
+# np.save("mnist_correct_preds.txt", y_test)
+
+# # TODO: Add usage example for outputting to emulation via rancutils.emulation.write_cores, et
